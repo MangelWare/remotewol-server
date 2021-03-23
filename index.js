3264e70fcd76d3b945091c694d4d28c1c371a406
Simon Mangel <simon.mangel@rwth-aachen.de>
2021-03-24 00:15:10 +0100

    Complete polling API, challenge-response authentication for WOL device, logger class, debug UI

--- index.js
+++ index.js
@@ -1,74 +1,73 @@
+const DEBUG = true;
+
 const express = require("express");
 const app = express();
-
 const fs = require("fs");
 const crypto = require("crypto");
 
-let key;
+const AuthCache = require("./lib/authCache");
+const authCache = new AuthCache();
+
+const WOLStateLogger = require("./lib/wolStateLogger");
+const wolStateLogger = new WOLStateLogger();
+
+// Reading the key for the HMAC-based WOL device authentication
+let hmacKey;
 if (!fs.existsSync("./.secret")) {
   throw new Error("Key file does not exist!");
 } else {
-  key = Buffer.from(fs.readFileSync("./.secret").toString(), "base64");
-  if (key.length != 64) {
+  hmacKey = Buffer.from(fs.readFileSync("./.secret").toString(), "base64");
+  if (hmacKey.length != 64) {
     throw new Error("Badly formatted key!");
   }
 }
 
-const DEBUG = true;
-if (DEBUG) {
-  //const key = crypto.randomBytes(64);
-  //console.log(key.toString("hex"));
-
-  /*key = Buffer.from(
-    "63ba6a3a0fbd4b38639c48a29eed85fdb3add1c7484c8cb4f4ccf9b13a436af48a88297a9acef170d71f0974691993cbc48347efca4f596e199bc99f25351cc6",
-    "hex"
-  );*/
-
-  let ts = Date.now();
-  const hmac = crypto.createHmac("SHA512-256", key);
-  hmac.update(ts.toString(), "utf-8");
-  let mac = hmac.digest("base64");
-  console.log(`Authorization: ${ts}:${mac}`);
-}
-
+/**
+ * Polling API for the WOL Device.
+ * Uses a common challenge-response authentication based on 512-bit
+ * randoms and HMAC (SHA512-256).
+ */
 app.get("/poll", (req, res) => {
   // Check if authorization header is present
   const { authorization } = req.headers;
   if (!authorization) {
-    res.status(403);
-    res.send("No authorization given!");
+    const newRandom = crypto.randomBytes(64).toString("base64");
+    authCache.add(newRandom);
+    res.send(newRandom);
+
+    if (DEBUG) {
+      const hmac = crypto.createHmac("SHA512-256", hmacKey);
+      hmac.update(Buffer.from(newRandom, "base64"));
+      const mac = hmac.digest("base64");
+      console.log(`Valid authorization:\n${newRandom}:${mac}`);
+    }
+
     return;
   }
 
   // Parse authorization header
-  let [timestamp, mac] = authorization.split(":");
-  if (!timestamp || !mac) {
+  const [random, mac] = authorization.split(":");
+  if (!random || !mac) {
     res.status(400);
     res.send("Authorization was not in the correct format!");
     return;
   }
 
-  // Parse timestamp as number
-  timestamp = Number(timestamp);
-  if (!timestamp || typeof timestamp !== "number" || timestamp == NaN) {
-    res.status(400);
-    res.send("Timestamp is not a number!");
+  // Check if random is correct
+  if (!authCache.has(random)) {
+    res.status(403);
+    res.send("Invalid random!");
     return;
   }
 
-  // Check timestamp recency
-  let now = Date.now();
-  if (now < timestamp || now - timestamp > 1000 * 10) {
-    res.status(403);
-    res.send("Timestamp already expired or in the future!");
-    return;
-  }
+  // Prevent replay attack by invalidating random
+  authCache.remove(random);
 
   // Verify MAC
-  let macInput = Buffer.from(timestamp.toString(), "utf8");
-  const hmac = crypto.createHmac("SHA512-256", key);
+  const macInput = Buffer.from(random, "base64");
+  const hmac = crypto.createHmac("SHA512-256", hmacKey);
   hmac.update(macInput);
-  let newMac = hmac.digest("base64");
+  const newMac = hmac.digest("base64");
   if (mac !== newMac) {
     res.status(403);
     res.send("MAC was not correct!");
@@ -76,7 +75,21 @@ app.get("/poll", (req, res) => {
   }
 
   // Authorized
-  res.send("Moooooin Meister!");
+  res.send(
+    wolStateLogger.poll({ debug: true }) ? "Wake up!" : "Go back to sleep!"
+  );
+});
+
+const uiPage = fs.readFileSync("./ui.html");
+app.get("/ui", (req, res) => {
+  res.type(".html");
+  res.send(uiPage);
+});
+
+app.post("/wakeup", (req, res) => {
+  res.send();
+  console.log("Received wakeup!");
+  wolStateLogger.wakeup({ debugUI: true });
 });
 
 app.listen(8080, () => {
